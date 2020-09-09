f093e86f94d69c9c6140d8454c48a39f4ed40f97
bolshakov <iddqdeika@gmail.com>
2020-09-09 11:24:42 +0300

    init

---
+++ client.go
@@ -0,0 +1,124 @@
+package pim
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"time"
+)
+
+const (
+	restUrlPath  = "/rest/V1.0/list/"
+	retryTimeout = time.Second
+)
+
+type Config struct {
+	Host          string
+	Login         string
+	Password      string
+	TimeoutInSecs int
+}
+
+func NewClient(config Config) (*Client, error) {
+	httpClient := http.Client{
+		Transport:     http.DefaultTransport,
+		CheckRedirect: nil,
+		Jar:           nil,
+		Timeout:       time.Second * time.Duration(config.TimeoutInSecs),
+	}
+
+	c := &Client{
+		Config: config,
+		client: httpClient,
+	}
+	sgp := &StructureGroupProvider{
+		c: c,
+	}
+	c.sgp = sgp
+	return c, nil
+}
+
+type Client struct {
+	Config Config
+	client http.Client
+
+	sgp *StructureGroupProvider
+}
+
+func (c *Client) StructureGroupProvider() *StructureGroupProvider {
+	return c.sgp
+}
+
+func (c *Client) baseUrl() string {
+	return c.Config.Host + restUrlPath
+}
+
+func (c *Client) post(url string, data []byte) (*http.Response, error) {
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(data))
+	if err != nil {
+		return nil, err
+	}
+	return c.client.Do(req)
+}
+
+func (c *Client) get(url string) (*PimReadResponse, error) {
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+	req.SetBasicAuth(c.Config.Login, c.Config.Password)
+
+	res, err := c.doWithRetries(req, 3)
+
+	data, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+	r := &PimReadResponse{}
+	err = json.Unmarshal(data, r)
+	if err != nil {
+		return nil, err
+	}
+	return r, nil
+}
+
+func (c *Client) doWithRetries(req *http.Request, retries int) (*http.Response, error) {
+	var res *http.Response
+	var err error
+	for i := 0; i < retries; i++ {
+		res, err = c.try(req)
+		if err == nil {
+			return res, nil
+		}
+		time.Sleep(retryTimeout)
+	}
+	return nil, err
+}
+
+func (c *Client) try(req *http.Request) (*http.Response, error) {
+	res, err := c.client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	if res.StatusCode != 200 {
+		return nil, fmt.Errorf("status code is %v", res.StatusCode)
+	}
+	return res, nil
+}
+
+type PimReadResponse struct {
+	Rows []PimReadRow `json:"rows"`
+}
+
+type PimReadRow struct {
+	Object PimReadObject `json:"object"`
+	Values []interface{} `json:"values"`
+}
+
+type PimReadObject struct {
+	ID       string `json:"id"`
+	EntityID int    `json:"entityId"`
+}
---
+++ infomodel.go
@@ -0,0 +1,85 @@
+package pim
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+const (
+	InfomodelPath = "StructureGroup/StructureGroupAttribute"
+)
+
+var (
+	//fields for requests
+	InfomodelFields = []string{"StructureGroupAttributeLang.Name(Russian)", "StructureGroupAttribute.Datatype", "StructureGroupAttributeLang.DomainValue(Russian)"}
+
+	//errors
+	TypeCastErr = fmt.Errorf("cant cast value to correct type")
+)
+
+type StructureGroup struct {
+	Identifier  string
+	StructureID int
+	Features    map[string]Feature
+}
+
+type Feature struct {
+	Name         string
+	DataType     string
+	PresetValues []string
+}
+
+type StructureGroupProvider struct {
+	c *Client
+}
+
+func (i *StructureGroupProvider) GetInfomodelByIdentifier(identifier string, structureID int) (*StructureGroup, error) {
+	url := i.c.baseUrl() + InfomodelPath + "/byItems?" +
+		"items=" + "'" + identifier + "'@" + strconv.Itoa(structureID) +
+		"&fields=" + strings.Join(InfomodelFields, ",") +
+		"&pageSize=-1"
+	res, err := i.c.get(url)
+	if err != nil {
+		return nil, err
+	}
+	fs := make(map[string]Feature)
+	for _, row := range res.Rows {
+		if len(row.Values) != len(InfomodelFields) {
+			return nil, fmt.Errorf("cant parse infomodel, wrong num of values in a row")
+		}
+		name, ok := row.Values[0].(string)
+		if !ok {
+			return nil, TypeCastErr
+		}
+		dataType, ok := row.Values[1].(string)
+		if !ok {
+			return nil, TypeCastErr
+		}
+		pi, ok := row.Values[2].([]interface{})
+		if !ok {
+			return nil, TypeCastErr
+		}
+		presets := make([]string, 0)
+		for _, vi := range pi {
+			val, ok := vi.(string)
+			if !ok {
+				return nil, TypeCastErr
+			}
+			if val == "" {
+				continue
+			}
+			presets = append(presets, val)
+		}
+		fs[name] = Feature{
+			Name:         name,
+			DataType:     dataType,
+			PresetValues: presets,
+		}
+	}
+	return &StructureGroup{
+		Identifier:  identifier,
+		StructureID: structureID,
+		Features:    fs,
+	}, nil
+}
