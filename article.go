ec9ec01e4230a2868240cb0c0f91c81823f86979
bolshakov <iddqdeika@gmail.com>
2020-09-09 17:57:23 +0300

    article work

--- article.go
+++ article.go
@@ -1,13 +1,25 @@
 package pim
 
+import ()
+
 const (
-	ArticleWithAttributesPath = "Article/ArticleAttribute"
+	ArticlePath           = "Article"
+	ArticleAttributesPath = "Article/ArticleAttribute"
 )
+
+var ()
 
 type ArticleProvider struct {
 	c *Client
 }
 
-func (p *ArticleProvider) MappingAndAttributesByIdentifier(articleIdentifier string, structureIdentifier string) {
-
+func (p *ArticleProvider) GetStructureMaps(articleIdentifier string, structureIdentifier string) []string {
+	url := p.c.baseUrl() + ArticlePath + "/byItems?" +
+		"items=" + "'" + articleIdentifier + "'@1" +
+		"&fields=ArticleStructureMap.StructureGroup(" + structureIdentifier + ")" +
+		"&pageSize=-1"
+	res, err := p.c.get(url)
+	if err != nil {
+		return nil, err
+	}
 }
