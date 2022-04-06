99856c0403a45792332cfb22053f75ca09a2d6f9
akondratev <artem.kondratev@goods.ru>
2022-04-06 13:38:52 +0300

    chore: убрал константы типов из пакета

--- const.go
+++ const.go
@@ -16,26 +16,4 @@ const (
 	ArticleMediaAssetMapPath = "Article/ArticleMediaAssetMap"
 	InfomodelPath            = "StructureGroup/StructureGroupAttribute"
 	StructureGroupPath       = "StructureGroup"
-
-	AssetMediaTypeDocID1 = iota - 3
-	AssetMediaTypeDocID2
-	AssetMediaTypeDocID3
-	AssetMediaTypeImageID1
-	AssetMediaTypeImageID2
-	AssetMediaTypeImageID3
-	AssetMediaTypeImageID4
-	AssetMediaTypeImageID5
-	AssetMediaTypeImageID6
-	AssetMediaTypeImageID7
-	AssetMediaTypeImageID8
-	AssetMediaTypeImageID9
-	AssetMediaTypeImageID10
 )
-
-func IsValidAssetMediaTypeID(id int) bool {
-	if id < AssetMediaTypeDocID1 || id > AssetMediaTypeImageID10 {
-		return false
-	}
-
-	return true
-}
